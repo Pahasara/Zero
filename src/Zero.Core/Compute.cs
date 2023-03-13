3553d09173833cfe9976753bf817b8b7560200db
PAHASARA DvNET <46932317+Pahasara@users.noreply.github.com>
2023-03-13 18:34:00 +0530

    Added library file

---
+++ src/Zero.Core/Compute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zero.Core
+{
+    public class Compute
+    {
+        public int getScroll_X(int currentRow, int maxRows, int progressBarMaxWidth, int LENGTH_FIX)
+        {
+            progressBarMaxWidth -= LENGTH_FIX;
+            Double rowPercentage = currentRow / Convert.ToDouble(maxRows - 1);
+            int x = ((int)(rowPercentage * progressBarMaxWidth) + LENGTH_FIX);
+            return x;
+        }
+
+        public int getProgressValue(int progressBarValue, double progressBarMaxLength)
+        {
+            double percentage = (progressBarValue / Convert.ToDouble(progressBarMaxLength));
+            percentage *= 100;
+            return (int)percentage;
+        }
+    }
+}
