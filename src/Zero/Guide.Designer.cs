3991ceab75a413b2c4e300dacf799d6e8b7bdc31
PAHASARA DvNET <46932317+Pahasara@users.noreply.github.com>
2023-03-25 18:49:31 +0530

    Added new form for beginnerGuide

---
+++ src/Zero/Guide.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Zero
+{
+    partial class Guide
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Guide));
+            this.btnNext = new System.Windows.Forms.PictureBox();
+            this.btnBack = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNext.Image = global::Zero.Properties.Resources.btnGuideNext;
+            this.btnNext.Location = new System.Drawing.Point(280, 131);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(24, 24);
+            this.btnNext.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnNext.TabIndex = 74;
+            this.btnNext.TabStop = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.Image = global::Zero.Properties.Resources.btnGuideBack;
+            this.btnBack.Location = new System.Drawing.Point(6, 131);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(24, 24);
+            this.btnBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnBack.TabIndex = 75;
+            this.btnBack.TabStop = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Image = global::Zero.Properties.Resources.btnGuideClose;
+            this.btnClose.Location = new System.Drawing.Point(281, 6);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(24, 24);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnClose.TabIndex = 76;
+            this.btnClose.TabStop = false;
+            this.btnClose.Visible = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Guide
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.BackgroundImage = global::Zero.Properties.Resources.guide_00;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(310, 160);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnNext);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Guide";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zero";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Guide_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.btnNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox btnNext;
+        private System.Windows.Forms.PictureBox btnBack;
+        private System.Windows.Forms.PictureBox btnClose;
+    }
+}
---
+++ src/Zero/Guide.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) 2023 Dewnith Fernando @github.com/Pahasara.
+// Licensed under the MIT license.
+
+using System;
+using System.Windows.Forms;
+
+namespace Zero
+{
+    public partial class Guide : Form
+    {
+        public Guide()
+        {
+            InitializeComponent();
+        }
+
+        int slide = 0, numberOfSlides = 5;
+
+        private void Guide_Load(object sender, EventArgs e)
+        {
+            changeSlide();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            slide++;
+            changeSlide();
+        }
+
+        private void changeSlide()
+        {
+            if (slide <= numberOfSlides)
+            {
+                show(btnBack);
+                show(btnNext);
+                btnNext.Image = Properties.Resources.btnGuideNext;
+                if (slide == 0)
+                {
+                    BackgroundImage = Properties.Resources.guide_00;
+                    hide(btnBack);
+                }
+                else if (slide == 1)
+                {
+                    BackgroundImage = Properties.Resources.guide_01;
+                    show(btnClose);
+                }
+                else if (slide == 2)
+                {
+                    BackgroundImage = Properties.Resources.guide_02;
+                }
+                else if (slide == 3)
+                {
+                    BackgroundImage = Properties.Resources.guide_03;
+                }
+                else if (slide == 4)
+                {
+                    BackgroundImage = Properties.Resources.guide_04;
+                }
+                else
+                {
+                    BackgroundImage = Properties.Resources.guide_05;
+                    hide(btnNext);
+                }
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            slide--;
+            changeSlide();
+        }
+
+        private void show(Control component)
+        {
+            component.Visible = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void hide(Control component)
+        {
+            component.Visible = false;
+        }
+    }
+}
