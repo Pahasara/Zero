58c6a374278b86b374b31500c20c5baca3a0a9a9
PAHASARA DvNET <46932317+Pahasara@users.noreply.github.com>
2023-03-25 18:50:09 +0530

    Updated readability of source

--- src/Zero.Core/Compute.cs
+++ src/Zero.Core/Compute.cs
@@ -1,30 +1,32 @@
-﻿using System;
-using System.Xml.Schema;
+﻿// Copyright (c) 2023 Dewnith Fernando @github.com/Pahasara.
+// Licensed under the MIT license.
+
+using System;
 
 namespace Zero.Core
 {
     public class Compute
     {
-        public int getScrollBarLocation(int currentRow, int maxRows, int progressBarMaxWidth, int LENGTH_FIX)
+        public int getScrollBarLocation(int currentRow, int maxRows, int maxLength, int LENGTH_FIX)
         {
-            progressBarMaxWidth -= LENGTH_FIX;
+            maxLength -= LENGTH_FIX;
             double rowPercentage = currentRow / Convert.ToDouble(maxRows - 1);
-            int x = ((int)(rowPercentage * progressBarMaxWidth) + LENGTH_FIX);
+            int x = ((int)(rowPercentage * maxLength) + LENGTH_FIX);
             return x;
         }
 
-        public int getPercentage(int progressBarValue, double progressBarMaxLength)
+        public int getPercentage(int length, double maxLength)
         {
-            double percentage = (progressBarValue / Convert.ToDouble(progressBarMaxLength));
+            double percentage = (length / Convert.ToDouble(maxLength));
             percentage *= 100;
             return (int)percentage;
         }
 
-        public int getScrollBarLength(int mRows)
+        public int getScrollBarLength(int maxRows)
         {
             double maxLength = 202;
             int minLength = 20;
-            double length = maxLength / mRows;
+            double length = maxLength / maxRows;
             if (length < minLength) 
                 length = minLength;
             return (int) length;
