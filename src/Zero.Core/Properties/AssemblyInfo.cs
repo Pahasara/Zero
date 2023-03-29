ff91b201e2f4a6c26e96f7679960e534de3a130c
PAHASARA DvNET <46932317+Pahasara@users.noreply.github.com>
2023-03-29 13:27:06 +0530

    Updated build settings

--- src/Zero.Core/Properties/AssemblyInfo.cs
+++ src/Zero.Core/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Resources;
 
 // General Information
@@ -9,12 +11,12 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dewnith Fernando")]
 [assembly: AssemblyProduct("Zero TVSM")]
-[assembly: AssemblyCopyright("©Dewnith Fernando. All rights reserved.")]
+[assembly: AssemblyCopyright("© Dewnith Fernando. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
 // Version information
-[assembly: AssemblyVersion("2.0.44.814")]
-[assembly: AssemblyFileVersion("2.0.44.814")]
+[assembly: AssemblyVersion("2.0.128.817")]
+[assembly: AssemblyFileVersion("2.0.128.817")]
 
--- src/Zero/Properties/AssemblyInfo.cs
+++ src/Zero/Properties/AssemblyInfo.cs
@@ -2,20 +2,22 @@
 
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Resources;
 
 // General Information
 [assembly: AssemblyTitle("DvNET Zero")]
-[assembly: AssemblyDescription("A smart ui to manage ur tv shows")]
+[assembly: AssemblyDescription("A smart ui to organize ur tv shows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dewnith Fernando")]
 [assembly: AssemblyProduct("Zero TVSM")]
-[assembly: AssemblyCopyright("©Dewnith Fernando. All rights reserved.")]
+[assembly: AssemblyCopyright("© Dewnith Fernando. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
 // Version information
-[assembly: AssemblyVersion("2.0.44.814")]
-[assembly: AssemblyFileVersion("2.0.44.814")]
+[assembly: AssemblyVersion("2.0.128.817")]
+[assembly: AssemblyFileVersion("2.0.128.817")]
 
